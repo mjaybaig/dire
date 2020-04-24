45910bf72a59a4b4aa2ebd78c09e6591a7d5206b
M. Jameel Baig <mjameelbaig@hotmail.com>
2020-04-25 00:27:27 +1000

    tensorflowjs integrated

--- Screens/CameraScreen.js
+++ Screens/CameraScreen.js
@@ -4,8 +4,14 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
 import * as tf from "@tensorflow/tfjs";
+import * as jpeg from "jpeg-js"
+
+import { FileSystem } from "react-native-unimodules";
+// import * as fs from "react-native-fs";
 
 import Colors from '../constants/Color'
+import { decodeJpeg, fetch } from '@tensorflow/tfjs-react-native';
+
 
 class CameraScreen extends React.Component{
   constructor(props){
@@ -14,12 +20,12 @@ class CameraScreen extends React.Component{
 
     this.state = {
       isTfReady: false,
-      pickedImage: null
+      pickedImage: null,
+      isModelReady: false
     }
 
   }
   async componentDidMount(){
-
     tf.ready().then(
       val => {
         this.setState({
@@ -29,8 +35,18 @@ class CameraScreen extends React.Component{
       }, reject => {
         console.log(reject)
       }
-      )
-    }
+    )
+    tf.loadLayersModel('../mlmodel/model.json').then(machineClassifer => {
+      this.setState({
+        machineClassifer: machineClassifer,
+        isModelReady: true
+      });
+      console.log(this.state.machineClassifer);
+    }, err => {
+      console.log(err);
+    })
+    
+  }
     
     //if user did/cancel give permision he wont be asked again
   verifyPermissions = async () => {
@@ -46,6 +62,53 @@ class CameraScreen extends React.Component{
     }
     return true;
   };
+  
+  imagetoTensor = rawImageData => {
+    const TO_UINT8ARRAY = true
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
+    console.log(width)
+    // Drop the alpha channel info for mobilenet
+    const buffer = new Uint8Array(width * height * 3)
+    let offset = 0 // offset into original data
+    for (let i = 0; i < buffer.length; i += 3) {
+      buffer[i] = data[offset]
+      buffer[i + 1] = data[offset + 1]
+      buffer[i + 2] = data[offset + 2]
+      offset += 4
+    }
+    console.log("hello")
+    return tf.tensor3d(buffer, [height, width, 3])
+  }
+
+  predictMachine = async () => {
+    // const image = await fs.readFile(this.state.pickedImage.uri);
+    // var rawImageData = jpeg.decode(image)
+    // console.log(rawImageData);
+    // console.log(image);
+    try{
+      // const image = await FileSystem.readAsStringAsync(this.state.pickedImage.uri, {encoding: FileSystem.EncodingType.Base64});
+      const imageAssetPath = Image.resolveAssetSource(this.state.pickedImage);
+      console.log(imageAssetPath);
+      const response = await fetch(imageAssetPath.uri, {}, {isBinary: true});
+      // console.log(response);
+      // const rawImageData = await im.arrayBuffer()
+      const rawImageData = await response.arrayBuffer();
+      console.log(rawImageData);
+      // console.log(image)
+      const imageTensor = this.imagetoTensor(rawImageData);
+      // console.log(imageTensor);
+      console.log(this.state.machineClassifer);
+      console.log(this.state.machineClassifer.predict(imageTensor))
+    }
+    catch(err){
+      console.log(err);
+    }
+      // this.state.machineClassifer.classify(imgTensor).then(predictions => {
+      //   console.log(predictions);
+      // });
+    // await fetch(this.state.pickedImage, {}, {isBinary: true});
+
+  }
 
   takeImageHandler = async () => {
     const hasPermission = await this.verifyPermissions();
@@ -58,10 +121,10 @@ class CameraScreen extends React.Component{
       aspect: [16, 9],
       quality: 0.5
     });
-    console.log(image)
     this.setState({
-      pickedImage: image.uri
+      pickedImage: {uri: image.uri}
     })
+    this.predictMachine();
     //props.onImageTaken(image.uri);
   }
 
@@ -72,7 +135,7 @@ class CameraScreen extends React.Component{
         {!this.state.pickedImage ? (
           <Text>No image picked yet.</Text>
           ) : (
-            <Image style={this.styles.image} source={{ uri: this.state.pickedImage }} />
+            <Image style={this.styles.image} source={{ uri: this.state.pickedImage.uri }} />
             )}
       </View>
       <Button
